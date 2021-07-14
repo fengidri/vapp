e064a823299b592bdb28cfd88b3c008dc529d33c
Xuan Zhuo <xuanzhuo@linux.alibaba.com>
2021-07-14 16:31:59 +0800

    format for vhost_vring_addr

--- vhost.h
+++ vhost.h
@@ -19,8 +19,14 @@ enum {
 struct vhost_vring_state { unsigned int index, num; };
 struct vhost_vring_file { unsigned int index; int fd; };
 struct vhost_vring_addr {
-  unsigned int index, flags;
-  uint64_t desc_user_addr, used_user_addr, avail_user_addr, log_guest_addr;
+  unsigned int index;
+  unsigned int flags;
+  uint64_t desc_user_addr;
+  uint64_t used_user_addr;
+  uint64_t avail_user_addr;
+  uint64_t log_guest_addr;
+
+
 };
 
 
